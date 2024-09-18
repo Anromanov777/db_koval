79677583c46f33bcd5f75c3a6c31d2b4390a6547
Andrei Romanov <you@example.com>
2024-09-18 19:57:14 +0300

    Удаление и изменение типа

--- src/db/Main.java
+++ src/db/Main.java
@@ -7,20 +7,93 @@ import java.sql.Statement;
 
 public class Main {
     static Statement statement;
+
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("org.sqlite.JDBC");
         Connection conn = DriverManager.getConnection("jdbc:sqlite:F:\\JAVA\\dbe\\My_cats.db");
         statement = conn.createStatement();
         int a = statement.executeUpdate("CREATE TABLE if not exists types(id INTEGER PRIMARY KEY AUTOINCREMENT,type VARCHAR(100) NOT NULL)");
-        System.out.println("Затронуто строк "+ insert_type("Абиссинская кошка"));
-        System.out.println("Затронуто строк "+ insert_type("Австралийский мист"));
-        System.out.println("Затронуто строк "+ insert_type("Американская жесткошерстная"));
+        delete_type(7);
+        update_type(9,"Чеширский кот");
 
     }
 
     private static int insert_type(String type) throws SQLException {
         String s = "INSERT INTO types (type) VALUES('" + type + "')";
-      int a =  statement.executeUpdate(s);
+        int a = statement.executeUpdate(s);
         return a;
     }
+
+    private static void update_type(int id, String new_type) throws SQLException {
+        String s = "UPDATE types SET type='" + new_type + "' WHERE id=" + id + ";";
+        statement.executeUpdate(s);
+    }
+
+    private static void delete_type(int id) throws SQLException {
+        String s = "DELETE FROM types WHERE id =" + id + ";";
+        statement.executeUpdate(s);
+    }
+
+    static String[] types = new String[]{"Абиссинская кошка",
+            "Австралийский мист",
+            "Американская жесткошерстная",
+            "Американская короткошерстная",
+            "Американский бобтейл",
+            "Американский кёрл",
+            "Балинезийская кошка",
+            "Бенгальская кошка",
+            "Бирманская кошка",
+            "Бомбейская кошка",
+            "Бразильская короткошёрстная",
+            "Британская длинношерстная",
+            "Британская короткошерстная",
+            "Бурманская кошка",
+            "Бурмилла кошка",
+            "Гавана",
+            "Гималайская кошка",
+            "Девон-рекс",
+            "Донской сфинкс",
+            "Европейская короткошерстная",
+            "Египетская мау",
+            "Канадский сфинкс",
+            "Кимрик",
+            "Корат",
+            "Корниш-рекс",
+            "Курильский бобтейл",
+            "Лаперм",
+            "Манчкин",
+            "Мейн-ку́н",
+            "Меконгский бобтейл",
+            "Мэнкс кошка",
+            "Наполеон",
+            "Немецкий рекс",
+            "Нибелунг",
+            "Норвежская лесная кошка",
+            "Ориентальная кошка",
+            "Оцикет",
+            "Персидская кошка",
+            "Петерболд",
+            "Пиксибоб",
+            "Рагамаффин",
+            "Русская голубая кошка",
+            "Рэгдолл",
+            "Саванна",
+            "Селкирк-рекс",
+            "Сиамская кошка",
+            "Сибирская кошка",
+            "Сингапурская кошка",
+            "Скоттиш-фолд",
+            "Сноу-шу",
+            "Сомалийская кошка",
+            "Тайская кошка",
+            "Тойгер",
+            "Тонкинская кошка",
+            "Турецкая ангорская кошка",
+            "Турецкий ван",
+            "Украинский левкой",
+            "Чаузи",
+            "Шартрез",
+            "Экзотическая короткошерстная",
+            "Японский бобтейл"
+    };
 }
