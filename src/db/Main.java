5e16050f7f338d49c25a02653aafdc6e654a9810
Andrei Romanov <you@example.com>
2024-09-18 19:18:56 +0300

    Первый INSERT

--- src/db/Main.java
+++ src/db/Main.java
@@ -6,11 +6,21 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Main {
+    static Statement statement;
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("org.sqlite.JDBC");
         Connection conn = DriverManager.getConnection("jdbc:sqlite:F:\\JAVA\\dbe\\My_cats.db");
-        Statement statement = conn.createStatement();
+        statement = conn.createStatement();
         int a = statement.executeUpdate("CREATE TABLE if not exists types(id INTEGER PRIMARY KEY AUTOINCREMENT,type VARCHAR(100) NOT NULL)");
-        System.out.println("Затронуто строк "+a);
+        System.out.println("Затронуто строк "+ insert_type("Абиссинская кошка"));
+        System.out.println("Затронуто строк "+ insert_type("Австралийский мист"));
+        System.out.println("Затронуто строк "+ insert_type("Американская жесткошерстная"));
+
+    }
+
+    private static int insert_type(String type) throws SQLException {
+        String s = "INSERT INTO types (type) VALUES('" + type + "')";
+      int a =  statement.executeUpdate(s);
+        return a;
     }
 }
