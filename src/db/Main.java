47389bd87d6988c478970d35e8ada9fad57098b0
Andrei Romanov <you@example.com>
2024-09-19 13:18:31 +0300

    Первый INSERT(ВТОРОЙ)

--- src/db/Main.java
+++ src/db/Main.java
@@ -1,21 +1,47 @@
 package db;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     static Statement statement;
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Scanner sc = new Scanner(System.in);
         Class.forName("org.sqlite.JDBC");
         Connection conn = DriverManager.getConnection("jdbc:sqlite:F:\\JAVA\\dbe\\My_cats.db");
         statement = conn.createStatement();
-        //int a = statement.executeUpdate("CREATE TABLE if not exists types(id INTEGER PRIMARY KEY AUTOINCREMENT,type VARCHAR(100) NOT NULL)");
         int a = statement.executeUpdate("CREATE TABLE if not exists cats(id INTEGER PRIMARY KEY AUTOINCREMENT,name VARCHAR(20) NOT NULL,type_id INTEGER NOT NULL REFERENCES types(id),age INTEGER NOT NULL,weight DOUBLE)");
         delete_type(7);
-        update_type(9,"Чеширский кот");
-        //System.out.println(get_type(15));
-        get_type_where("type LIKE '%п'");
-        get_all_types();
+        update_type(9, "Чеширский кот");
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Введите кличку кота: ");
+            String name = sc.nextLine();
+            System.out.print("Введите породу кота: ");
+            String type_id = sc.nextLine();
+            System.out.print("Введите возраст кота: ");
+            int age = sc.nextInt();
+            System.out.print("Введите вес кота: ");
+            double weight = sc.nextDouble();
+            sc.nextLine();
+            insert_cat(name, type_id, age, weight);
+        }
+    }
+
+    private static void insert_cat(String name, String type, int age, Double weight) throws SQLException {
+        ResultSet resultSet = statement.executeQuery("SELECT id FROM types WHERE type = '" + type + "'");
+        if (resultSet.next()) {
+            String s = "INSERT INTO cats (name,type_id,age,weight) VALUES ('" + name + "'," + resultSet.getInt(1) + "," + age + "," + weight + ")";
+            statement.executeUpdate(s);
+        } else {
+            String s = "INSERT INTO types(type) VALUES ('" + type + "')";
+            statement.executeUpdate(s);
+            s = "SELECT id FROM types WHERE type = '" + type + "';";
+            ResultSet resultSet1 = statement.executeQuery(s);
+            s = "INSERT INTO cats (name,type_id,age,weight) VALUES ('" + name + "'," + resultSet1.getInt(1) + "," + age + "," + weight + ")";
+            statement.executeUpdate(s);
+        }
     }
 
     private static int insert_type(String type) throws SQLException {
@@ -34,25 +60,25 @@ public class Main {
         statement.executeUpdate(s);
     }
 
-    private  static String get_type(int id) throws SQLException {
-        String s ="SELECT type FROM types WHERE id="+id;
-        ResultSet a= statement.executeQuery(s);
+    private static String get_type(int id) throws SQLException {
+        String s = "SELECT type FROM types WHERE id=" + id;
+        ResultSet a = statement.executeQuery(s);
         return a.getString(1);
     }
 
-    private  static void get_type_where(String where) throws SQLException {
-        String s ="SELECT type FROM types WHERE "+where+";";
-        ResultSet a= statement.executeQuery(s);
-        while (a.next()){
+    private static void get_type_where(String where) throws SQLException {
+        String s = "SELECT type FROM types WHERE " + where + ";";
+        ResultSet a = statement.executeQuery(s);
+        while (a.next()) {
             System.out.println(a.getString("type"));
         }
     }
 
-    private  static void get_all_types() throws SQLException {
-        String s ="SELECT type FROM types;";
-        ResultSet a= statement.executeQuery(s);
-        while (a.next()){
-            System.out.println(a.getRow()+" "+a.getString("type"));
+    private static void get_all_types() throws SQLException {
+        String s = "SELECT type FROM types;";
+        ResultSet a = statement.executeQuery(s);
+        while (a.next()) {
+            System.out.println(a.getRow() + " " + a.getString("type"));
         }
     }
 
